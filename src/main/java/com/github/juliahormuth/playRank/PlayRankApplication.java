907e2290e5d140966d933af6478e305d332c9bfd
Julia Hormuth <jvhormuth@gmail.com>
2023-07-17 21:26:19 -0300

    first commit

---
+++ src/main/java/com/github/juliahormuth/playRank/PlayRankApplication.java
@@ -0,0 +1,13 @@
+package com.github.juliahormuth.playRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayRankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/juliahormuth/playRank/PlayRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliahormuth.playRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
